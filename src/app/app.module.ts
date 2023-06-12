df2b2db68de4debe45978c53818d85734deb2561
Abolins Aizbalts <r.abolins.aizbalts@accenture.com>
2023-06-12 18:51:58 +0300

    Created models, services folders and generated all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileCardComponent } from './profile-card/profile-card.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ProfilePopupComponent } from './profile-popup/profile-popup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileCardComponent,
+    NavigationComponent,
+    ProfilePopupComponent,
+    DashboardComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+  ],
+  exports: [
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
